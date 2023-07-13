998f5d9a0cb79c856448d19aa4631b01a140e91d
nat485 <nat485@gmail.com>
2023-07-13 10:42:24 -0500

    Adding the rest of the code to match the concessions ID that is listed.

--- Test/concessions.jest.js
+++ Test/concessions.jest.js
@@ -3,4 +3,18 @@ const concessions = require("../data/concessions");
 const {
     concessionID,
     addTotalFromIDs,
-  } = require("../src/concessions");
+} = require("../src/concessions");
+
+describe("concessionID()", () => {
+    test("These are the ID's that match the concession objects", () => {
+        const actual = concessionID(concessions, "g9sZdG1hI");
+        const expected = { id: "g9sZdG1hI", name: "Small Popcorn", priceInCents: 929 };
+        expect(actual).toStrictEqual(expected);
+    });
+    test("Return `null` if there is no object that matches.", () => {
+        const actual = concessionID(concessions, "g9sZpQ1hI");
+        const expected = null;
+        expect(actual).toBe(expected);
+    });
+});
+
